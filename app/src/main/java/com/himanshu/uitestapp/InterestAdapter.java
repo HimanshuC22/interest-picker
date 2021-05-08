f423673656a1da2d2d1bc29e19ca867380903a10
Himanshu Choudhary <22himanshu14@gmail.com>
2021-05-08 16:32:46 +0530

    Added Android Studio Project

---
+++ app/src/main/java/com/himanshu/uitestapp/InterestAdapter.java
@@ -0,0 +1,58 @@
+package com.himanshu.uitestapp;
+
+import android.content.Context;
+import android.os.TestLooperManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class InterestAdapter extends BaseAdapter {
+    public Context context;
+    public String[] values;
+
+    InterestAdapter(Context context, String[] textValues)
+    {
+        this.context = context;
+        this.values = textValues;
+    }
+
+    @Override
+    public int getCount() {
+        return values.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View gridView;
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from mobile.xml
+            gridView = inflater.inflate(R.layout.textview_item, null);
+
+            // set value into textview
+            TextView textView;
+            textView = (TextView) gridView
+                    .findViewById(R.id.grid_item_label);
+            textView.setText(values[position]);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+}
